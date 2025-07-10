80b473a67d02d6f7564630c66397a7a447eb0447
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2025-07-10 15:43:13 +0200

    Extension yang-data: Minor fixes

--- yangson/schemanode.py
+++ yangson/schemanode.py
@@ -56,7 +56,7 @@ from .exceptions import (
     AnnotationTypeError, InvalidXPath, InvalidLeafrefPath,
     MissingAnnotationTarget, MissingModuleNamespace, RawMemberError,
     RawTypeError, SchemaError, SemanticError, UndefinedAnnotation,
-    InvalidStatement, YangsonException, YangTypeError)
+    InvalidStatement, YangsonException, YangTypeError, InvalidArgument)
 from .instance import (ArrayEntry, InstanceNode, MemberName, ObjectMember)
 from .instroute import InstanceRoute
 from .instvalue import (
@@ -1034,6 +1034,14 @@ class YangData(GroupNode):
 
         set_ctype(self)
         self.children[0]._post_process()
+
+        stn = self.parent
+        for c in stn.children:
+            if isinstance(c, YangData) and c.children[0].name == self.children[0].name:
+                raise InvalidArgument("ietf-restconf:yang-data container names collide.")
+
+            elif c.name == self.children[0].name:
+                raise InvalidArgument("ietf-restconf:yang-data container and toplevel container names collide.")
 
     def _schema_pattern(self: "YangData") -> SchemaPattern:
         return self._pattern_entry()
