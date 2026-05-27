8de5a21d8fca38c87554198e96c9ba53e1fa2cb1
Vojtech Vilimek <vojtech.vilimek@nic.cz>
2026-05-27 15:13:52 +0200

    Move InstanceRoute into instance, implement as_schema_route()

--- yangson/instance.py
+++ yangson/instance.py
@@ -16,6 +16,8 @@
 # along with Yangson.  If not, see <http://www.gnu.org/licenses/>.
 
 """Instance data represented as a persistent structure.
+Instance route: A route into an instance value that is also used as the cooked
+value of the instance-identifier type.
 
 This module implements the following classes:
 
@@ -30,11 +32,14 @@ This module implements the following classes:
 * EntryValue: Instance route selector for value-based access to array entries.
 * EntryKeys: Instance route selector for keys-based access to array entries.
 * InstanceIdParser: Parser for instance identifiers.
+
+* InstanceRouteItem: Protocol class defining API for instance route items.
+* InstanceRoute: Route into an instance value.
 """
 from collections import deque
 from datetime import datetime
 import json
-from typing import Mapping, Optional, TYPE_CHECKING, Union
+from typing import Mapping, Optional, TYPE_CHECKING, Union, Protocol
 from urllib.parse import unquote
 import xml.etree.ElementTree as ET
 from .enumerations import ContentType, ValidationScope
@@ -1304,7 +1309,7 @@ class ResourceIdParser(Parser):
 class InstanceIdParser(Parser):
     """Parser for YANG instance identifiers."""
 
-    def parse(self) -> InstanceRoute:
+    def parse(self) -> "InstanceRoute":
         """Parse instance identifier."""
         res = []
         if self.input == "/":
@@ -1355,6 +1360,59 @@ class InstanceIdParser(Parser):
                 break
             self.skip_ws()
         return EntryKeys(sel)
+
+
+class InstanceRouteItem(Protocol):
+    """This protocol class defines a required API for instance route items."""
+
+    def __eq__(self, other: object) -> bool:
+        """Return True if the receiver is equal to other."""
+
+    def __str__(self) -> str:
+        """Return string representation of the receiver (i-i segment)."""
+
+    def peek_step(self, val: "StructuredValue",
+                  sn: "DataNode") -> tuple[Optional["Value"], "DataNode"]:
+        """Return value addressed by the receiver relative to the current
+        position together with its schema node.
+
+        Args:
+            val: Value at the current position.
+            sn:  Schema node corresponding to the current position.
+        """
+
+    def goto_step(self, inst: "InstanceNode") -> "InstanceNode":
+        """Return instance node addressed by the receiver.
+
+        Args:
+            inst: Current instance node.
+        """
+
+
+class InstanceRoute(tuple[InstanceRouteItem, ...]):
+    """This class represents a route into an instance value."""
+
+    def __str__(self) -> str:
+        """Return instance-id as the string representation of the receiver."""
+        if self:
+            return "".join([str(c) for c in self])
+        else:
+            return "/"
+
+    def __hash__(self) -> int:
+        """Return the hash value of the receiver."""
+        return self.__str__().__hash__()
+
+
+    def as_schema_route(self) -> "SchemaRoute":
+        """Get a SchemaRoute from InstanceRoute.
+        This ttransformation removes all key values and leaf-list/list indices."""
+        sch_route = []
+        for mem_key in self:
+            if isinstance(mem_key, (EntryKeys, EntryIndex, EntryValue)):
+                continue
+            sch_route.append(mem_key.as_qual_name())
+        return sch_route
 
 
 from .schemanode import (       # NOQA
